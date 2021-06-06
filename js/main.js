d8d8bd779b11ef517e71053a4a9c00dc9fcc95f0
Leanne Chan <54755036+LeanneChan@users.noreply.github.com>
2021-06-06 12:20:26 +0800

    Test image

--- js/main.js
+++ js/main.js
@@ -1,17 +1,28 @@
 // Add the Map
 
+
+// var map = L.map('map', {
+//   center: [1.344896, 103.8],
+//   zoom: 11
+// });
+
 var map = L.map('map', {
-  center: [1.344896, 103.8],
-  zoom: 11
+    crs: L.CRS.Simple,
+    minZoom: -5
 });
 
-var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
-  attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-  subdomains: 'abcd',
-  minZoom: 0,
-  maxZoom: 20,
-  ext: 'png'
-}).addTo(map);
+var bounds = [[0,0], [1000,1000]]
+var image = L.imageOverlay('Copy of Jingjing Card.png', bounds).addTo(map);
+
+
+// var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+//   attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+//   subdomains: 'abcd',
+//   minZoom: 0,
+//   maxZoom: 20,
+//   ext: 'png'
+// }).addTo(map);
+
 
 
 // messages 
